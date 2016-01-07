ab6d39e99e59f22056ea1f69b35b9b6c70b424c1
MangTomas23 <matos.adrianpaul@gmail.com>
2016-01-07 11:52:35 +0800

    Remove backquote/backticks if database driver is pgSQL

--- src/SearchableTrait.php
+++ src/SearchableTrait.php
@@ -281,6 +281,11 @@ trait SearchableTrait
      * @return string
      */
     protected function getCaseCompare($column, $compare, $relevance) {
+        if($this->getDatabaseDriver() == 'pgsql') {
+            $field = "LOWER(" . $column . ") " . $compare . " ?";    
+            return '(case when ' . $field . ' then ' . $relevance . ' else 0 end)';
+        }
+
         $column = str_replace('.', '`.`', $column);
         $field = "LOWER(`" . $column . "`) " . $compare . " ?";
         return '(case when ' . $field . ' then ' . $relevance . ' else 0 end)';
@@ -309,6 +314,9 @@ trait SearchableTrait
      * @param \Illuminate\Database\Eloquent\Builder $original
      */
     protected function mergeQueries(Builder $clone, Builder $original) {
+        if($this->getDatabaseDriver() == 'pgsql'){
+            $original->from(DB::connection($this->connection)->raw("({$clone->toSql()}) as {$this->getTable()}"));
+        }
         $original->from(DB::connection($this->connection)->raw("({$clone->toSql()}) as `{$this->getTable()}`"));
         $original->mergeBindings($clone->getQuery());
     }
